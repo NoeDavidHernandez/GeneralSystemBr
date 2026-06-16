eef77df03bc067ff398a0c8413b7bc2dcb954fda
noe2912 <noe.hernandez2235@uppuebla.edu.mx>
2026-06-16 17:43:39 -0600

    docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Configurar Apache para escuchar en el puerto que Render asigna dinámicamente
+if [ -n "$PORT" ]; then
+    sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
+fi
+
 # Esperar un par de segundos a que todo esté listo e iniciar migraciones
 echo "Corriendo migraciones..."
 php artisan migrate --force
